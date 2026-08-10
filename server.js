35de4bf7886bf5d3e18577d7f10a736d576f671d
omar <omardoughman@icloud.com>
2026-08-10 16:55:44 +0100

    Implement basic HTTP server in server.js

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
-'use strict';
+const http = require('http');
 
-module.exports = require('./server.node');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello from my Render app!\n');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
